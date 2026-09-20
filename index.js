1a211e2ab992ef1043820833c4e47ef819ab6218
Yuvraj Singh Chouhan <yuvrajsinghchouhan9669@gmail.com>
2026-09-20 20:21:44 +0530

    Displayed data from mongoDB

--- index.js
+++ index.js
@@ -6,17 +6,29 @@ const url = "mongodb://localhost:27017/"
 
 const client = new MongoClient(url);
 
-async function dbConnection(){
+// async function dbConnection(){
+//     await client.connect();
+//     const db = client.db(dbName);
+//     const Collection = db.collection('students')
+
+//     const result = await Collection.find().toArray()
+//     console.log(result); 
+// }
+// dbConnection()
+const app = express();
+
+
+// Displaying Data From MongoDB
+app.set("view engine","ejs");
+
+app.get('/',async(req,resp)=>{
     await client.connect();
     const db = client.db(dbName);
     const Collection = db.collection('students')
 
-    const result = await Collection.find().toArray()
-    console.log(result); 
-}
-dbConnection()
-const app = express();
-
-// Displaying Data from mongoDb
+    const students = await Collection.find().toArray()
+    console.log(students); 
+    resp.render('students',{students});
+})
 
 app.listen(3200);
